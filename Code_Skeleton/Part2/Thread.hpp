4412e48efe2f2bd397371c308eee847c0377281c
daniellek12 <danielle.kupitman@gmail.com>
2018-12-31 16:01:05 +0200

    Update Thread.hpp

--- Code_Skeleton/Part2/Thread.hpp
+++ Code_Skeleton/Part2/Thread.hpp
@@ -6,6 +6,7 @@ class Thread
 public:
 	Thread(uint thread_id) 
 	{
+		this->thread_id = thread_id;
 		// Only places thread_id 
 	} 
 	virtual ~Thread() {} // Does nothing 
@@ -13,17 +14,20 @@ public:
 	/** Returns true if the thread was successfully started, false if there was an error starting the thread */
 	bool start()
 	{
+		int res = pthread_create(&m_thread, nullptr, entry_func, this);
+		return (res == 0) ? true : false;
 	}
 
 	/** Will not return until the internal thread has exited. */
 	void join()
 	{
+		pthread_join(m_thread, nullptr);
 	}
 
 	/** Returns the thread_id **/
 	uint thread_id()
 	{
-
+		return this->thread_id;
 	}
 protected:
 	/** Implement this method in your subclass with the code you want your thread to run. */
